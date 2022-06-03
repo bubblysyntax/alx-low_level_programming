2f71bbd1dd041bfa664edeb6a14a03ea1eace42d
emmade86 <emmade86@gmail.com>
2022-06-03 04:49:30 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Entry Point
  *
- * Return: Always 0 (Success)
+ * Description: prints a string to the standard error
+ *
+ * Return: Always 1(Error)
  */
 int main(void)
 {
-	print(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is usefull" Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
