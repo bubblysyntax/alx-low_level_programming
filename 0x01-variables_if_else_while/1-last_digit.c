3fb4fdb304df404a0c1a42ec812395328e7452dd
bubblysyntax <nikkitarajaram12@gmail.com>
2022-06-03 17:35:19 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,12 +16,16 @@ int main(void)
 
 	ld = n % 10;
 	if (ld == 0)
-	{	
-		printf("Last digit of %i is %i is less than 6 and not 0\n" , n, ld);
+	{
+		printf("Last digit of %i is %i is less than 6 and not 0\n", n, ld);
+	}
+	else if (ld < 6 && ld ! = 0)
+	{
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, ld);
 	}
 	if (ld > 5)
 	{
-		printf("Last digit of i% is %i and is greater than 5\n" , n, ld);
+		printf("Last digit of %i is %i and is greater than 5\n", n, ld);
 	}
 	return (0);
 }
