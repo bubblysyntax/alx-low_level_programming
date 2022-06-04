6d1184831ab256ffaa5b0c2039ecc39cf4b4981e
bubblysyntax <nikkitarajaram12@gmail.com>
2022-06-03 17:42:03 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	{
 		printf("Last digit of %i is %i is less than 6 and not 0\n", n, ld);
 	}
-	else if (ld < 6 && ld ! = 0)
+	else if ((ld < 6 && ld) ! = 0)
 	{
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, ld);
 	}
