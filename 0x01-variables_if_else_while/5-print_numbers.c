b6ab821e72f7955d8ac89acd7ed2d440f111ae0d
bubblysyntax <nikkitarajaram12@gmail.com>
2022-06-05 18:31:19 -0400

    5 print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	int num = '0';
 	
-	for (num = '0'; num <= '9'; num++)
+	for (num = '0'; num < 10; num++)
 	{
-		printf("num");
+		printf("%d", num);
 	}
 	printf("\n");
 	return (0);
