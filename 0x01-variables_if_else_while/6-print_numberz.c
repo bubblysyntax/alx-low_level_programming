6699c82025939e2b19c66326357d9b1fc943cc88
bubblysyntax <nikkitarajaram12@gmail.com>
2022-06-05 20:12:01 -0400

    6 print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = '0';
+	
+	for (num = '0'; num <= 9; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
