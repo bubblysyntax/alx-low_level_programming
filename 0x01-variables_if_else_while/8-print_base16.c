3513a9afbac6e6bda68c073db5ec0e283eeaf245
bubblysyntax <nikkitarajaram12@gmail.com>
2022-06-05 21:50:45 -0400

    8 print base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = '0';
+	char alpha = 'a';
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	putchar('\n');
+	return (0);
+}
