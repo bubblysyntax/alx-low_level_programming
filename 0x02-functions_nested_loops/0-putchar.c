138b5981814e3fac1e532484015bf61060dafe6a
bubblysyntax <nikkitarajaram12@gmail.com>
2022-06-07 20:21:16 -0400

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Print "_putchar"
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int _putchar(char c);
 int main(void)
 {
-	int i = 0;
-	char c[] = "_putchar";
-
-	while (i < 8)
-	{
-		_putchar(c[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
